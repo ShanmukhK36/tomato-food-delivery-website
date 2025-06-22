03587c7a25746f8208562ee4237fce0b9fe51f16
ShanmukhK36 <katakamshanmukh@gmail.com>
2025-06-22 12:22:38 -0500

    Refactored Code

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,16 +1,19 @@
+import {v2 as cloudinary} from 'cloudinary';
 import foodModel from "../models/foodModel.js";
-import fs from 'fs';
 
 // add food items
 const addFood = async(req, res) => {
-    console.log(req.file);
-    let image_filename = `${req.file.filename}`;
+    let image = req.file.image && req.file.image[0];
+    let result = await cloudinary.uploader.upload(image.path, {
+        resource_type: 'image',
+    });
+    let image_url = result.secure_url;
     const food = new foodModel({
         name: req.body.name,
         description: req.body.description,
         price: req.body.price,
         category: req.body.category,
-        image: image_filename
+        image: image_url
     })
     try {
         await food.save();
@@ -35,8 +38,6 @@ const foodList = async(req, res) => {
 // remove food item
 const removeFood = async(req, res) => {
     try {
-        const food = await foodModel.findById(req.body.id);
-        fs.unlink(`uploads/${food.image}`, () => {});
         await foodModel.findByIdAndDelete(req.body.id);
         res.json({success: true, message: 'Food Removed'});
     } catch(error) {
