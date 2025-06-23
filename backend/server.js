92685d2dd44d7d2aee153c9d21b2038ba315821a
ShanmukhK36 <katakamshanmukh@gmail.com>
2025-06-22 20:55:17 -0500

    Refactored Code 6

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
 import { connectDB } from './config/db.js';
 import connectCloudinary from './config/cloudinary.js';
 import foodRouter from './routes/foodRoute.js';
