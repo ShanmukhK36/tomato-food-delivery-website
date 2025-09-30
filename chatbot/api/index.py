e04d8657a6921509fb1db2d8f5a56897871f5346
ShanmukhK36 <katakamshanmukh@gmail.com>
2025-09-30 15:18:09 -0500

    Added AI chatbot

--- chatbot/api/index.py
+++ chatbot/api/index.py
@@ -1,7 +1,2 @@
-# /chatbot/api/index.py
-import os, sys
-
-# Add parent folder (/chatbot) to import path so we can import main.py
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-from main import app  # FastAPI instance named "app"
+# Vercel will look for `app` here
+from chatbot.main import app
