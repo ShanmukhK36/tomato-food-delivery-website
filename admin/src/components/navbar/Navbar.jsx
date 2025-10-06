6c0b6e39b074c02cb101d2d7f8af9c8a2e22373e
ShanmukhK36 <katakamshanmukh@gmail.com>
2025-10-06 15:05:56 -0500

    Added AI chatbot

--- admin/src/components/navbar/Navbar.jsx
+++ admin/src/components/navbar/Navbar.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
 import './Navbar.css';
 import {assets} from '../../assets/assets';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const frontend_url = import.meta.env.VITE_FRONTEND_URL;
   return (
     <div className='navbar'>
         <img className='logo' src={assets.logo} />
-        <img className='profile' src={assets.profile_image} />
+        <Link to={frontend_url}>
+          <img className='profile' src={assets.profile_image} />
+        </Link>
     </div>
   )
 }
