3d6a2694446c3d77b54d111aa6aa9c2287a5129e
ShanmukhK36 <katakamshanmukh@gmail.com>
2025-10-17 18:18:54 -0500

    added changes for chatbot order

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -2,21 +2,24 @@ import userModel from '../models/userModel.js';
 
 // add items to user cart
 const addToCart = async (req, res) => {
-    try {
-        let userData = await userModel.findById(req.body.userId);
-        let cartData = await userData.cartData;
-        if(!cartData[req.body.itemId]) {
-            cartData[req.body.itemId] = 1;
-        } else {
-            cartData[req.body.itemId] += 1;
-        }
-        await userModel.findByIdAndUpdate(req.body.userId, {cartData});
-        res.json({success: true, message: 'Added To Cart'});
-    } catch(error) {
-        console.log(error);
-        return res.json({success: false, message: error.message});
+  try {
+    const qty = Number(req.body.quantity) || 1; // default = 1
+    const userData = await userModel.findById(req.body.userId);
+    const cartData = userData.cartData || {};
+
+    if (!cartData[req.body.itemId]) {
+      cartData[req.body.itemId] = qty;
+    } else {
+      cartData[req.body.itemId] += qty;
     }
-}
+
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: `Added ${qty} item(s) to cart` });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // remove items from user cart
 const removeFromCart = async (req, res) => {
